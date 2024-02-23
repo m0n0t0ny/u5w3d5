d2bbae0ad77650986607e98cc828e337f7a0e3a3
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-23 19:23:37 +0100

    Refactor `AuthService` and `UserService` methods for clarity and consistency.

--- src/main/java/u5w3d5/finalproject/services/AuthService.java
+++ src/main/java/u5w3d5/finalproject/services/AuthService.java
@@ -3,49 +3,45 @@ package u5w3d5.finalproject.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import u5w3d5.finalproject.Role;
+import u5w3d5.finalproject.entities.Role;
 import u5w3d5.finalproject.entities.User;
 import u5w3d5.finalproject.exceptions.BadRequestException;
 import u5w3d5.finalproject.exceptions.UnauthorizedException;
-import u5w3d5.finalproject.payload.user.NewUserDTO;
-import u5w3d5.finalproject.payload.user.UserLoginDTO;
+import u5w3d5.finalproject.payload.login.UserLoginDTO;
+import u5w3d5.finalproject.payload.user.UserDTO;
 import u5w3d5.finalproject.repositories.UserDAO;
 import u5w3d5.finalproject.security.JWTTools;
 
 @Service
 public class AuthService {
   @Autowired
-  private UserService usersService;
-
+  private UserService userService;
+  @Autowired
+  private JWTTools jwTtools;
+  @Autowired
+  private UserDAO userDAO;
   @Autowired
   private PasswordEncoder bcrypt;
 
-  @Autowired
-  private UserDAO usersDAO;
-
-  @Autowired
-  private JWTTools jwtTools;
-
-  public String authenticateUserAndGenerateToken(UserLoginDTO payload) {
-    User user = usersService.findByEmail(payload.email());
-    if (bcrypt.matches(payload.password(), user.getPassword())) {
-      return jwtTools.createToken(user);
+  public String authenticateUserAndGenerateToken(UserLoginDTO body) {
+    User user = userService.findByEmail(body.email());
+    if (bcrypt.matches(body.password(), user.getPassword())) {
+      return jwTtools.createToken(user);
     } else {
-      throw new UnauthorizedException("Credenziali sbagliate!");
+      throw new UnauthorizedException("Credenziali non valide!!");
     }
   }
 
-  public User saveUser(NewUserDTO payload) {
-    usersDAO.findByEmail(payload.email()).ifPresent(user -> {
-      throw new BadRequestException("L'indirizzo email '" + payload.email() + "' è già in uso");
+  public User save(UserDTO body) {
+    userDAO.findByEmail(body.email()).ifPresent(user -> {
+      throw new BadRequestException("email " + user.getEmail() + " già usata!!!");
     });
     User newUser = new User();
-    newUser.setEmail(payload.email());
-    newUser.setName(payload.name());
-    newUser.setSurname(payload.surname());
-    newUser.setEmail(payload.email());
-    newUser.setPassword(bcrypt.encode(payload.password()));
+    newUser.setName(body.name());
+    newUser.setSurname(body.surname());
+    newUser.setEmail(body.email());
+    newUser.setPassword(bcrypt.encode(body.password()));
     newUser.setRole(Role.USER);
-    return usersDAO.save(newUser);
+    return userDAO.save(newUser);
   }
 }
--- src/main/java/u5w3d5/finalproject/services/UserService.java
+++ src/main/java/u5w3d5/finalproject/services/UserService.java
@@ -1,37 +1,50 @@
 package u5w3d5.finalproject.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import u5w3d5.finalproject.entities.User;
 import u5w3d5.finalproject.exceptions.NotFoundException;
 import u5w3d5.finalproject.repositories.UserDAO;
 
+import java.util.List;
 import java.util.UUID;
 
 
 @Service
 public class UserService {
-
   @Autowired
   private UserDAO userDAO;
 
-  @Value("{$jwt.secret}")
-  private String jwtSecret;
-
-  public User registerUser(User user) {
-    return userDAO.save(user);
+  public List<User> getUsers() {
+    return this.userDAO.findAll();
   }
 
-  public String loginUser(String email, String password) {
-    return jwtSecret;
-  }
-
-  public User findById(UUID userId) {
-    return userDAO.findById(userId).orElseThrow(() -> new NotFoundException(userId));
+  public User findById(UUID id) {
+    return userDAO.findById(id).orElseThrow(() -> new NotFoundException(id));
   }
 
   public User findByEmail(String email) {
-    return userDAO.findByEmail(email).orElseThrow(() -> new NotFoundException("Email " + email + " non trovata"));
+    return userDAO.findByEmail(email).orElseThrow(() -> new NotFoundException("Utente con email " + email + " non trovata!!!"));
+  }
+
+  public User findByIdAndUpdate(UUID id, User body) {
+    User found = this.findById(id);
+    if (body.getName() != null) {
+      found.setName(body.getName());
+    }
+    if (body.getSurname() != null) {
+      found.setSurname(body.getSurname());
+    }
+    if (body.getEmail() != null) {
+      found.setEmail(body.getEmail());
+    }
+    if (body.getPassword() != null) {
+      found.setPassword(body.getPassword());
+    }
+    if (body.getRole() != null) {
+      found.setRole(body.getRole());
+    }
+
+    return userDAO.save(found);
   }
 }
