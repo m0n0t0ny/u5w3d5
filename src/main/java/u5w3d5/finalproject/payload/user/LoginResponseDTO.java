6d91f4cc61343f3cce75eff0bdef5b26b6bf1220
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-23 16:13:43 +0100

    Add NewUserDTO, LoginResponseDTO, and UserLoginDTO classes to payload package.

---
+++ src/main/java/u5w3d5/finalproject/payload/user/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package u5w3d5.finalproject.payload.user;
+
+public record LoginResponseDTO(String accessToken) {
+}
---
+++ src/main/java/u5w3d5/finalproject/payload/user/UserLoginDTO.java
@@ -0,0 +1,4 @@
+package u5w3d5.finalproject.payload.user;
+
+public record UserLoginDTO(String email, String password) {
+}
