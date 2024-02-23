37312538672c80501b4f302eedada42fefd028e2
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-23 16:07:54 +0100

    Rename UserRepository and EventRepository interfaces to UserDAO and EventDAO respectively, add existsByEmail method to UserDAO.

--- src/main/java/u5w3d5/finalproject/repositories/EventDAO.java
+++ src/main/java/u5w3d5/finalproject/repositories/EventDAO.java
@@ -1,14 +1,12 @@
 package u5w3d5.finalproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import u5w3d5.finalproject.entities.Event;
 import u5w3d5.finalproject.entities.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-
-  Optional<User> findByEmail(String email);
+public interface EventDAO extends JpaRepository<Event, UUID> {
+  Optional<Event> findById(UUID id);
 }
--- src/main/java/u5w3d5/finalproject/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package u5w3d5.finalproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import u5w3d5.finalproject.entities.Event;
-
-import java.util.UUID;
-
-public interface EventRepository extends JpaRepository<Event, UUID> {
-}
